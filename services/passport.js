0d6559899a3bab36a828acfccbd2066e2733d246
Ryan Gelow <exisphere@gmail.com>
2019-07-26 12:59:13 -0700

    configure jwt strategy

--- services/passport.js
+++ services/passport.js
@@ -36,8 +36,31 @@ const jwtOptions = {
     // Tells JWT Strategy that whenever a request comes in, we want passport to handle it
     // It needs to look in the header for the property called "authorization"
     jwtFromRequest: ExtractJwr.fromHeader('authorization'),
-
+    // Tells JWT Strategy what secret we used to encode the token so that it can decode it
     secretOrKEy: config.secret
 }
 
+// We are going to get the payload argument from an incoming request
+// The payload argument is coming from the function that we will create in authRoutes
+// done is the function we call once we tried to authenticate this user
+const JwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+    try {
+        const user = await User.findById(payload.sub);
+        if(user) {
+            done(null, user);
+        }else{
+            done(null, false)
+        }
+    } catch(e) {
+        done(e, false)
+    }
+})
+
+// This tells passport that we declared these strategies.
+// The local login says we have a stragey called "Local"
+// The jwtLogin login says we have a stragey called jwt
+
+// When we say passport.authenticate ('jwt'), passport will look for a strategy call jwt
+
 passport.use(localLogin);
+passport.use(jwtLogin)
