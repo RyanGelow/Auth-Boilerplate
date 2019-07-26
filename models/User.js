bf58bf7fa38f2014a8fd6cba0709dcb69793d951
Ryan Gelow <exisphere@gmail.com>
2019-07-26 11:04:45 -0700

    add encryption to user password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -22,6 +23,26 @@ const UserSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+UserSchema.pre('save', async function(next) {
+    // For clarification user about to be saved === "this"
+    const user = this;
+    try{ 
+        // Salt is random characters to help encrypt passwords -- one way street
+        const salt = await bcrypt.genSalt();
+        console.log('salt', salt);
+        // Hash is user password + Salt
+        const hash = await bcrypt.hash(user.password, salt);
+        console.log('hash', hash);
+        
+        user.password = hash;
+        next();
+
+    } catch(e){
+        return next(e);
+    }
+
 });
 
 const User = mongoose.model('User', UserSchema);
